2f0d4302c7e45c3fd8967903b36f9b85d978eac1
yaruliu <yarusophia.liu@mail.utoronto.ca>
2017-07-15 00:59:59 -0700

    css

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -9,17 +9,18 @@ const Login = () => {
           <nav>
             <div className="nav-right">
             <ul className="top">
+              <li>Create Account</li>
+              <li>
+              <Link to="/home">
+                  Sign In
+              </Link>
+              </li>
 
             </ul>
-
             </div>
           </nav>
 
           <section className="first-intro">
-            <a href="https://www.mattek.com/">
-              <img className="snoopy" src="https://s-media-cache-ak0.pinimg.com/736x/c0/d6/fc/c0d6fc395a99a07c0238b4f8d6be4b03--snoopy-wallpaper-iphone-wallpaper.jpg" alt="Snoopy" />
-            </a>
-
             <div className="head-icon">
               <span className="cat">ABOUT</span>
               <span className="cat">CATOGORIES</span>
@@ -29,55 +30,39 @@ const Login = () => {
 
             <div className="head-txt">
               <p>
-                Understanding
-              </p>
-              <p>
-                human biology in
+                Be a
               </p>
               <h1 className="dimension">
-                DIMENSIONS
-                <sup>3</sup>
+                Learner
               </h1>
-              <a href="https://www.mattek.com/" className="button button-white">find your science</a>
+              <a href="https://www.mattek.com/" className="button button-white">LEARN MORE</a>
+              <a href="https://www.mattek.com/" className="button button-white">LOG IN</a>
             </div>
           </section>
 
-          <section class="section">
-              <h2 class="big-paragraph">
-                Innovative in vitro science is what we do.
+          <section className="section">
+              <h2 className="big-paragraph">
+                Study and Teach what you can and can not imagine
               </h2>
 
-              <p class="small-paragraph">
-                Engineered human tissues and cell based assays at the forefront of in vitro testing technology.
-                Let’s do some innovating!
+              <p className="small-paragraph">
+                An "SESSION-BASED" "LIVE-STREAM" education platform that allows people to study and teach.
+                Anything!
               </p>
-          </section>
-
-          <section class="second-intro">
-            .....
           </section>
 
           <section className="second-intro">
               <div className="head-txt">
                 <p>
-                  Understanding
-                </p>
-                <p>
-                  human biology in
+                  Be a
                 </p>
                 <h1 className="dimension">
-                  DIMENSIONS
-                  <sup>3</sup>
+                  Teacher
                 </h1>
-                <a href="https://www.mattek.com/" className="button button-white">find your science</a>
+                <a href="https://www.mattek.com/" className="button button-white">LEARN MORE</a>
+                <a href="https://www.mattek.com/" className="button button-white">LOG IN</a>
               </div>
-              <div className="login-div">
-                <input type="text" name="username" className="form-control input-box" placeholder="username"/>
-                <input type="text" name="password" className="form-control input-box" placeholder="password"/>
-                <Link to="/home">
-                    <a href="https://www.mattek.com/" className="button button-white">LOG IN</a>
-                </Link>
-              </div>
+
           </section>
         </div>
     );
