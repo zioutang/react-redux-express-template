3ca9fe5f6477d18d900f252cd165167b51e15db2
yaruliu <yarusophia.liu@mail.utoronto.ca>
2017-07-14 23:16:57 -0700

    css

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -7,19 +7,11 @@ const Login = () => {
     return (
         <div>
           <nav>
-            <ul className="top">
-            <li id="number">+1 (508) 881-6771</li>
-            <li>Email Us</li>
-            <li>Information Request</li>
-            </ul>
             <div className="nav-right">
             <ul className="top">
-            <li>Create Account</li>
-            <li>Sign In</li>
+
             </ul>
-            <a href="#" className="top" id="cart">
-            <span className="glyphicon glyphicon-shopping-cart"></span> Cart
-            </a>
+
             </div>
           </nav>
 
@@ -29,13 +21,10 @@ const Login = () => {
             </a>
 
             <div className="head-icon">
-              <span>ABOUT</span>
-              <span>PRODUCT</span>
-              <span>APPLICATIONS</span>
-              <span>REFERENCE</span>
-              <span>NEWS</span>
-              <span>STORE</span>
-              <i id="search-icon" className="fa fa-search"></i>
+              <span className="cat">ABOUT</span>
+              <span className="cat">CATOGORIES</span>
+              <span className="cat">CONTACT US</span>
+              <span className="cat">JOIN US</span>
             </div>
 
             <div className="head-txt">
@@ -69,11 +58,24 @@ const Login = () => {
           </section>
 
           <section className="second-intro">
+              <div className="head-txt">
+                <p>
+                  Understanding
+                </p>
+                <p>
+                  human biology in
+                </p>
+                <h1 className="dimension">
+                  DIMENSIONS
+                  <sup>3</sup>
+                </h1>
+                <a href="https://www.mattek.com/" className="button button-white">find your science</a>
+              </div>
               <div className="login-div">
                 <input type="text" name="username" className="form-control input-box" placeholder="username"/>
                 <input type="text" name="password" className="form-control input-box" placeholder="password"/>
                 <Link to="/home">
-                    <input type="submit" value="Login"/>
+                    <a href="https://www.mattek.com/" className="button button-white">LOG IN</a>
                 </Link>
               </div>
           </section>
